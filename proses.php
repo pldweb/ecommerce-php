73360e482f1dadfd23d17bd1419ed99ebdc8f590
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2024-11-27 15:51:10 +0700

    Updte sql

--- proses.php
+++ proses.php
@@ -3,25 +3,31 @@
 include 'src/database.php';
 
 $database = new Database();
-$aksi = $_GET['aksi'];
 
-// Data User
-$nama = $_POST['nama'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$alamat = $_POST['alamat'];
-$nomor_telp = $_POST['nomor_telp'];
+if (isset($_GET['aksi'])) {
+    // Data User
 
-if ($aksi == 'tambah') {
-    $database->tambahUser($nama, $email, $password, $alamat, $nomor_telp);
-    header("location:index.php");
-} elseif ($aksi == 'update') {
-    $database->updateUser($_POST['id'], $nama, $email, $password, $alamat, $nomor_telp);
-    header("location:index.php");
-} elseif ($aksi == 'delete') {
-    $database->deleteUser($_POST['id']);
-    header("location:index.php");
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $alamat = $_POST['alamat'];
+    $nomor_telp = $_POST['nomor_telp'];
+
+    if ($_GET['aksi'] == 'tambah') {
+        $database->tambahUser($nama, $email, $password, $alamat, $nomor_telp);
+        header("location:index.php");
+    } elseif ($_GET['aksi'] == 'update') {
+        $database->updateUser($_POST['id'], $nama, $email, $password, $alamat, $nomor_telp);
+        header("location:index.php");
+    } elseif ($_GET['aksi'] == 'delete') {
+        $database->deleteUser($_POST['id']);
+        header("location:index.php");
+    } else {
+        echo 'Ngga tau aksi apa';
+    }
 } else {
-    echo 'Belum Ada';
+    echo "Tidak ada aksi";
 }
+
+
 ?>
--- src/table.sql
+++ src/table.sql
@@ -15,3 +15,61 @@ CREATE TABLE user (
     FOREIGN KEY (role_id) REFERENCES role(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE kategori_produk
+(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nama VARCHAR(100),
+    parent_id INT DEFAULT NULL,
+    FOREIGN KEY (parent_id) REFERENCES kategori_produk (id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE brand_produk
+(
+    id         INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nama       VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE produk
+(
+    id          INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nama        VARCHAR(100),
+    brand_id    INT       DEFAULT NULL,
+    kategori_id INT       DEFAULT NULL,
+    harga       VARCHAR(255),
+    diskon      VARCHAR(15),
+    stok        INTEGER,
+    deskripsi   TEXT,
+    foto        VARCHAR(255),
+    FOREIGN KEY (brand_id) REFERENCES brand_produk (id),
+    FOREIGN KEY (kategori_id) REFERENCES kategori_produk (id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE transaksi(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    user_id INT DEFAULT NULL,
+    ongkir VARCHAR(255),
+    total VARCHAR(255),
+    status VARCHAR(255),
+    foreign key (user_id) references user(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE detail_transaksi
+(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    transaksi_id INT DEFAULT NULL,
+    produk_id INT DEFAULT NULL,
+    harga_produk VARCHAR(255) DEFAULT NULL,
+    foreign key (produk_id) references produk(id),
+    foreign key (transaksi_id) references transaksi(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
