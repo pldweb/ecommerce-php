cc8765514c4e44feb385383b4029993a2d1ef982
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2024-12-01 13:45:11 +0700

    Update MVC

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+
+define('BASE_URL', 'http://ecommerce.it');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '12345');
+define('DB_NAME', 'ecommerce');
--- app/models/User_model.php
+++ app/models/User_model.php
@@ -2,41 +2,6 @@
 
 class User_model {
 
-    private $dbh; // Database Handler
-    private $stmt;
-
-    public function __construct()
-    {
-        // Data Source Name
-        $dsn = 'mysql:host=localhost;dbname=ecommerce;';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '12345');
-        } catch (PDOException $e) {
-            echo 'Connection DB failed: ' . $e->getMessage();
-        }
-    }
-
-    // Contoh struktur data
-    private $nama = [
-        [
-            "id" => 1,
-            "nama" => "Yusri",
-            "email" => "yusri@gmail.com",
-            "alamat" => "Condet",
-            "nomor_telp" => "0323513123",
-            "role_id" => "user",
-        ],
-        [
-            "id" => 2,
-            "nama" => "Jojo",
-            "email" => "jojo@gmail.com",
-            "alamat" => "Bandar Gebang",
-            "nomor_telp" => "12312413231",
-            "role_id" => "user",
-        ],
-    ];
-
     public function getUser()
     {
         $this->stmt = $this->dbh->prepare("SELECT * FROM user");
