09a69e976b04a8f8c915ea4d9a5bce257d357cea
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2024-12-05 02:39:21 +0700

    Update confifg

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,22 @@
 <?php
 
-define('BASE_URL', 'http://ecommerce.it');
-define('SCRIPT_PATH', BASE_URL . '/app/assets');
+if ($_SERVER['SERVER_NAME'] == 'ecommerce.it') {
+    define('BASE_URL', 'http://ecommerce.it');
 
-// DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '12345');
-define('DB_NAME', 'ecommerce');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '12345');
+    define('DB_NAME', 'ecommerce');
+} else {
+    define('BASE_URL', 'https://ecommerce.paldidesign.my.id');
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'ecok6667_root');
+    define('DB_PASS', '#Adm!n-123#');
+    define('DB_NAME', 'ecok6667_ecommerce');
+}
+
+define('SCRIPT_PATH', BASE_URL . '/app/assets');
 
 define('DEFAULT_PAGINATION', 25);
 define('TIMEZONE', 'Asia/Jakarta');
