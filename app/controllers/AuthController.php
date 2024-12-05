8d330ac2e504b3c30fed28af84680f2e966da8dc
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2024-12-06 01:44:03 +0700

    Update

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -56,17 +56,6 @@ class Auth extends Controller
             header('location:' . BASE_URL . '/auth/login');
             exit;
         }
-
-
-//        $data['login'] = $this->model('AuthModel')->getData($email, $password);
-//        if ($data['login'] == NULL) {
-//            header("location:" . BASE_URL . "/login");
-//        } else {
-//            foreach ($data['login'] as $sesi) {
-//                $_SESSION['nama'] = $sesi['nama'];
-//                header("location:" . BASE_URL . "/user");
-//            }
-//        }
     }
 
     public function daftar()
@@ -76,45 +65,59 @@ class Auth extends Controller
 
     public function prosesdaftar()
     {
-        if (strlen($_POST['nama']) == 0) {
+        $nama = $_POST['nama'];
+        if (strlen(strval($nama)) == 0) {
+            Flasher::setflash('Gagal', 'nama tidak ada', 'danger');
+            header('location:' . BASE_URL . '/auth/daftar');
+            exit;
+        }
+
+        $email = $_POST['email'];
+        if (strlen(strval($email)) == 0) {
             Flasher::setflash('Gagal', 'email tidak ada', 'danger');
             header('location:' . BASE_URL . '/auth/daftar');
             exit;
         }
 
-        if (strlen($_POST['email']) == 0) {
-            Flasher::setflash('Gagal', 'email tidak ada', 'danger');
-            header('location:' . BASE_URL . '/auth/daftar');
-            exit;
-        }
-
-        if (strlen($_POST['password']) == 0) {
+        $password = $_POST['password'];
+        if (strlen(strval($password)) == 0) {
             Flasher::setflash('Gagal', 'password tidak ada', 'danger');
             header('location:' . BASE_URL . '/auth/daftar');
             exit;
         }
-//        $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-        if (strlen($_POST['nomor_telp']) == 0) {
+        $nomor_telp = $_POST['nomor_telp'];
+        if (strlen(strval($nomor_telp)) == 0) {
             Flasher::setflash('Gagal', 'no telp tidak ada', 'danger');
             header('location:' . BASE_URL . '/auth/daftar');
             exit;
         }
 
-        if ($_POST) {
-            $data['daftar'] = $this->model('AuthModel')->daftar($_POST);
-            if ($data['daftar'] == true) {
-                foreach ($data['daftar'] as $sesi) {
-                    $_SESSION['nama'] = $sesi['nama'];
-                }
-                Flasher::setFlash('Berhasil', 'Selamat Anda berhasil daftar', 'success');
-                header('location:' . BASE_URL . '/user');
-                exit;
-            } else {
-                Flasher::setFlash('Gagal', 'Data Anda tidak bisa didaftarkan', 'danger');
-                header('location:' . BASE_URL . '/auth/daftar');
-                exit;
-            }
+        $db = new Database();
+        $db->dbh->beginTransaction();
+
+        try {
+
+            $query = "INSERT INTO user (nama, email, password, nomor_telp) 
+              VALUES (:nama, :email, :password, :nomor_telp)";
+
+            $db->query($query);
+            $db->bind('nama', $nama);
+            $db->bind('email', $email);
+            $db->bind('password', $password);
+            $db->bind('nomor_telp', $nomor_telp);
+            $db->execute();
+            $db->dbh->commit();
+
+            Flasher::setflash('Berhasil', 'Anda berhasil login', 'success');
+            header('location:' . BASE_URL . '/login');
+            exit;
+
+        } catch (PDOException $e) {
+            $db->dbh->rollback();
+            Flasher::setflash('Gagal' . $e->getMessage(), 'Terjadi Kesalahan', 'danger');
+            header('location:' . BASE_URL . '/auth/daftar');
+            exit;
         }
     }
 
